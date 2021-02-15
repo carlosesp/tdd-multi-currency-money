10aa6dcf10e7a1cf9907c95bdc8bc4c8009f9093
Carlos Espiritu <carlosespiritu.pe@gmail.com>
2021-02-15 18:38:41 -0500

    Chapter 08. Create objects. Use factory methods to decoupled reference to subclasses in test code. Eliminate duplication of signature of times() method, and move its declaration to the superclass.

---
+++ chapter08/src/main/java/pe/carlosesp/tddmoney/chapter08/Money.java
@@ -0,0 +1,21 @@
+package pe.carlosesp.tddmoney.chapter08;
+
+abstract class Money {
+    protected int amount;
+
+    public static Money dollar(int amount) {
+        return new Dollar(amount);
+    }
+
+    public static Money franc(int amount) {
+        return new Franc(amount);
+    }
+
+    public boolean equals(Object object) {
+        Money money = (Money) object;
+        return amount == money.amount
+                && getClass().equals(money.getClass());
+    }
+
+    abstract Money times(int multiplier);
+}
---
+++ chapter08/src/test/java/pe/carlosesp/tddmoney/chapter08/MoneyTest.java
@@ -0,0 +1,32 @@
+package pe.carlosesp.tddmoney.chapter08;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MoneyTest {
+
+    @Test
+    public void testMultiplication() {
+        Money dollar = Money.dollar(5);
+        assertEquals(Money.dollar(10), dollar.times(2));
+        assertEquals(Money.dollar(15), dollar.times(3));
+    }
+
+    @Test
+    public void testEquality() {
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
+        assertFalse(Money.franc(5).equals(Money.franc(6)));
+        assertFalse(Money.franc(5).equals(Money.dollar(5)));
+    }
+
+    @Test
+    public void testFrancMultiplication() {
+        Money franc = Money.franc(5);
+        assertEquals(Money.franc(10), franc.times(2));
+        assertEquals(Money.franc(15), franc.times(3));
+    }
+
+}
